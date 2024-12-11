63b2bed2484e0c4c9ca2fb75ebdd83754cda417d
Rajul Mishra <166211612+rajulm70@users.noreply.github.com>
2024-12-12 01:43:05 +0530

    Update AdminDashboard.js

--- client/src/pages/Admin/AdminDashboard.js
+++ client/src/pages/Admin/AdminDashboard.js
@@ -5,7 +5,7 @@ import { useAuth } from "../../context/auth";
 const AdminDashboard = () => {
   const [auth] = useAuth();
   return (
-    <Layout title={'AdminDashboard - ShopSphere'}>
+    <Layout title={'AdminDashboard - AI-PrepHub'}>
       <div className="container-fluid m-3 p-3">
         <div className="row">
           <div className="col-md-3">
