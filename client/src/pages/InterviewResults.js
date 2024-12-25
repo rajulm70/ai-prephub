92117e825dfc726c523a2056312acf25f6972672
Rajul Mishra <166211612+rajulm70@users.noreply.github.com>
2024-12-25 23:33:26 +0530

    Update InterviewResults.js
    Update styling

--- client/src/pages/InterviewResults.js
+++ client/src/pages/InterviewResults.js
@@ -32,35 +32,35 @@ export default function InterviewResults() {
   return (
     <Layout title={'Interview Result'}>
       <div className="container container-int">
-      <h1 className="mt-4" style={{"textAlign":"center"}}>Interview Result</h1>
-      <p style={{"textAlign":"center"}}>Your mock interview is complete. Here's a summary of your performance:</p>
+        <h1 className="mt-4" style={{ "textAlign": "center" }}>Interview Result</h1>
+        <p style={{ "textAlign": "center" }}>Your mock interview is complete. Here's a summary of your performance:</p>
 
-      {/* Display result based on average score */}
-      <div className={resultClass + ' mt-3'}>
-        <h4>Your Score: {averageScore}%</h4>
-        <p>{resultText}</p>
+        {/* Display result based on average score */}
+        <div className={resultClass + ' mt-3'}>
+          <h4>Your Score: <span style={{"fontWeight":"bold"}}>{averageScore}%</span></h4>
+          <p>{resultText}</p>
+        </div>
+
+        <div className="list-group mt-4">
+          {evaluations.map((evaluation, index) => (
+            <div key={index} className="list-group-item list-group-item-int">
+              <h5><b>Q{index + 1}:</b><span style={{"color":"#1E1B4B"}}> {evaluation.question}</span> </h5>
+              <p ><strong  style={{"color":"#880808"}}>Score: </strong><span style={{"color":"#FF4433"}}>{evaluation.evaluation}/10</span></p>
+              <p ><strong style={{"color":"#FFBF00"}}>Feedback: </strong>{evaluation.feedback}</p>
+              <p><strong style={{"color":"#50C878"}}>Your Answer: </strong>{evaluation.transcription}</p>
+
+              <hr style={{ "color": "black" }} />
+            </div>
+          ))}
+        </div>
+
+        <div className="text-center mt-4">
+          <button className="btn btn-primary mb-5 btn-submit-interview" onClick={() => navigate('/interview')}>
+            &nbsp;&nbsp;Try Again?&nbsp;&nbsp;
+          </button>
+        </div>
       </div>
-
-      <div className="list-group mt-4">
-        {evaluations.map((evaluation, index) => (
-          <div key={index} className="list-group-item list-group-item-int">
-            <h5><b>Q{index + 1}:</b> {evaluation.question}</h5>
-            <p><strong>Score: </strong>{evaluation.evaluation}/10</p>
-            <p><strong>Feedback: </strong>{evaluation.feedback}</p>
-            <p><strong>Your Answer: </strong>{evaluation.transcription}</p>            
-
-            <hr style={{"color" : "black"}} />
-          </div>
-        ))}
-      </div>
-
-      <div className="text-center mt-4">
-        <button className="btn btn-primary mb-5 btn-submit-interview" onClick={() => navigate('/interview')}>
-        &nbsp;&nbsp;Try Again?&nbsp;&nbsp;
-        </button>
-      </div>
-    </div>
     </Layout>
-    
+
   );
 }
